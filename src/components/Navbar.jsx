8650306d2d0d3ecd7829c9ec11ccb305195d97ff
Yenuli Minsadi <samararatney@gmail.com>
2025-12-27 14:34:53 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
 
 const Navbar = () => {
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const { user, setUser, setShowUserLogin } = useAppContext();
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
       <NavLink to={"/"}>
@@ -21,9 +23,9 @@ const Navbar = () => {
 
       {/* Desktop Menu */}
       <div className="hidden sm:flex items-center gap-8">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/poducts">All Products</NavLink>
+        <NavLink to="/">Contact</NavLink>
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-400 px-3 rounded-full">
           <input
@@ -106,21 +108,37 @@ const Navbar = () => {
           open ? "flex" : "hidden"
         } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
       >
-        <a href="#" className="block">
+        <NavLink to="/" onClick={() => setOpen(false)}>
           Home
-        </a>
-        <a href="#" className="block">
-          About
-        </a>
-        <a href="#" className="block">
+        </NavLink>
+        <NavLink to="/products" onClick={() => setOpen(false)}>
+          All Products
+        </NavLink>
+        {user && (
+          <NavLink to="/products" onClick={() => setOpen(false)}>
+            My Orders
+          </NavLink>
+        )}
+        <NavLink to="/" onClick={() => setOpen(false)}>
           Contact
-        </a>
-        <button className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
-          Login
-        </button>
+        </NavLink>
+
+        {!user ? (
+          <button onClick={()=>{
+            setOpen(false);
+            setShowUserLogin(true);
+          }} className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
+            Login
+          </button>
+        ) : (
+          <button className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm"
+          >
+            Logout
+          </button>
+        )}
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
