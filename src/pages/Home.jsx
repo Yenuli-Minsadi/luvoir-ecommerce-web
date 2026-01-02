06e78b711f8940b773993db5c6e6b86f592cf8d5
Yenuli Minsadi <samararatney@gmail.com>
2026-01-02 09:12:59 +0530

    update with footer section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,19 @@ import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
 import NewsLetter from "../components/NewsLetter";
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
-    <div className='mt-10'>
-        <MainBanner />
-        <Categories />
-        <BestSeller />
-        <BottomBanner />
-        <NewsLetter />
+    <div className="mt-10">
+      <MainBanner />
+      <Categories />
+      <BestSeller />
+      <BottomBanner />
+      <NewsLetter />
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Home
