a0aa60fb427c9f3343c392ec2dd3064718ecda7a
Yenuli Minsadi <samararatney@gmail.com>
2026-01-03 21:11:05 +0530

    update with login form

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const {showUserLogin} = useAppContext()
   return (
     <>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
       <div
         className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
       >
